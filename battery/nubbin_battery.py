ff8598f1658d469b52291eb776c26ab805c771ff
Qianruy <qianruy99@gmail.com>
2022-06-29 20:34:00 -0400

    unit test commit

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,17 +1,11 @@
-from abc import ABC
-
-from car import Car
-
-from datetime import datetime
+from battery.battery import Battery
 
 
-class NubbinBattery(Car, ABC):
-    def __init__(self, last_service_date):
-        super().__init__(last_service_date)
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
 
-    def needs_serviced(self):
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.needs_serviced():
-            return True
-        else:
-            return False
+        return service_threshold_date < self.current_date
--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,13 +1,11 @@
-from abc import ABC
-
-from car import Car
+from engine.engine import Engine
 
 
-class SternmanEngine(Car, ABC):
+class SternmanEngine(Engine):
     def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
-    def needs_serviced(self):
+    def needs_service(self):
         if self.warning_light_is_on:
             return True
         else:
